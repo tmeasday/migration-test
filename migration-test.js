a586791b2c1a7566ae003bb31c6583ce635e3c85
Tom Coleman <tom@thesnail.org>
2014-10-10 16:24:53 +1100

    Repro

--- migration-test.js
+++ migration-test.js
@@ -1,64 +1,26 @@
-if (Meteor.isServer) {
-  var Flips = new Meteor.Collection('flips');
-  var Responses = new Meteor.Collection('responses');
-  var Comments = new Meteor.Collection('comments');
-
-  var onePerRecord = function(cursor, name, fn) {
-    if (! _.isFunction(fn))
-      throw "Must pass function into `onePerRecord`";
-
-    var count = cursor.count();
-    Log('Updating ' + count + ' ' + name + 's');
-
-    var i = 0;
-    cursor.forEach(function(record) {
-      i += 1;
-      if (i % 100 === 0)
-        Log('Up to the ' + i + 'th ' + name);
+if (! Meteor.isServer)
+  return;
   
-      return fn(record);
-    });
+var Documents = new Mongo.Collection('documents');
 
-    Log('Completed updating ' + + i + ' - ' + count + ' ' + name + 's');
-  }
-  
-  Meteor.startup(function () {
-    onePerRecord(Flips.find({lastActivity: {$exists: false}}), 'flip', function(flip) {
-
-      var lastResponse = Responses.findOne({flipId: flip._id}, {sort: {createdAt: -1}});
-
-      var selector = {responseId: {$in: Responses.find({flipId: flip._id}).map(function(r) {return r._id;})}};
-      var lastComment = Comments.findOne(selector, {sort: {createdAt: -1}});
-
-      var activity;
-      if (lastResponse && (! lastComment || lastResponse.createdAt > lastComment.createdAt)) {
-        activity = {
-          type: 'added-response',
-          id: lastResponse._id,
-          text: lastResponse.body,
-          userId: lastResponse.studentId,
-          at: lastResponse.createdAt
-        }
-      } else if (lastComment) {
-        activity = {
-          type: 'added-comment',
-          id: lastComment._id,
-          text: lastComment.body,
-          userId: lastComment.commenterId,
-          at: lastComment.createdAt
-        }
-      } else {
-        activity = {
-          type: 'added-flip',
-          id: flip._id,
-          text: flip.title,
-          userId: flip.teacherId,
-          at: flip.createdAt
-        }
-      }
-
-      Flips.update({_id: flip._id, lastActivity: {$exists: false}},
-        {$set: {lastActivity: activity}});
-    });
+// create some >4mb documents
+if (Documents.find().count() === 0) {
+  var bigStr = _.times(1024 * 1024, function() { return '01234'}).join();
+  _.times(10, function() {
+    Documents.insert({
+      str: bigStr
+    })
   });
 }
+
+Log("There are " + Documents.find().count() + " documents");
+
+var i = 0;
+Documents.find().forEach(function() {
+  i += 1;
+  // >10 minutes
+  Meteor._sleepForMs(15 * 60 * 1000);
+  Log(i);
+});
+
+Log("The loop ran " + i + " times");
